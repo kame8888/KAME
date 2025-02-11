3faf61806d8948f6f41e7ede38b0785e54b14a6e
kame8888 <happychacha@jttk.zaq.ne.jp>
2025-02-11 14:50:16 +0900

    Update main.py

--- main-2.py
+++ main-2.py
@@ -2,14 +2,11 @@ import multiprocessing as mp
 
 import centerCam
 import frontCam
-import controller
 
 #----------------------------------------------------------
 if __name__ == '__main__':
     print('start')
     p1 = mp.Process(target=centerCam.centerCam)
-    p2 = mp.Process(target=frontCam.frontCam)
-    p3 = mp.Process(target=controller.control)
+    p2 = mp.Process(target=frontCam.frontCam）
     p1.start()
-    p2.start()
-    p3.start()
+    p2.start(）
